ba33c6ffd0ce05b03eda76095204be110263a8e5
larijuva <lari.juva@hoiwa.fi>
2023-01-13 21:49:03 +0200

    Req changes

--- backend/routes/addExpense.js
+++ backend/routes/addExpense.js
@@ -4,8 +4,8 @@ const expenses = require('../models/expenses')
 
 router.get('/', async (req, res, next) => {
   const amount = req.query.amount
-  const shop = req.query.shop_id
-  const category = req.query.category_id
+  const shop = req.query
+  const category = req.query
   const date = req.query.date
   const month = req.query.month
   const year = req.query.year
